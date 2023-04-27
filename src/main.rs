3de8d9d1221e451f2eb0272fe0a401cf6bbcd045
hazadus <hazadus7@gmail.com>
2023-04-27 14:56:08 +0300

    Matching with Option<T>

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,12 @@ fn main() {
     println!("home = {:?}, loopback = {:?}", home, loopback);
 
     value_in_cents(Coin::Quarter(UsState::Alaska));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("{:?} {:?} {:?}", five, six, none);
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -42,5 +48,12 @@ fn value_in_cents(coin: Coin) -> u8 {
             println!("State quarter from: {:?}", state);
             25
         }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
