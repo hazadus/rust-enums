93e65f3a9876a1cf085166d3773e3aee656ecebb
hazadus <hazadus7@gmail.com>
2023-04-27 14:52:10 +0300

    Patterns that bind to valeus

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,36 @@ enum IpAddress {
     V6(String),
 }
 
+#[derive(Debug)]
+enum UsState{
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
     let home = IpAddress::V4(127, 0, 0, 1);
     let loopback = IpAddress::V6(String::from("::1"));
 
     println!("home = {:?}, loopback = {:?}", home, loopback);
+
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from: {:?}", state);
+            25
+        }
+    }
 }
